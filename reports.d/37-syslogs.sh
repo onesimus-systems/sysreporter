02fcb31c36d397364d12c4a60724ffb47499c44f
Lee Keitel <keitellf@gmail.com>
2015-12-11 15:00:04 -0600

    Syslog report to use journalctl or /var/log/messages as alt sources

--- reports.d/37-syslogs.sh
+++ reports.d/37-syslogs.sh
@@ -2,5 +2,13 @@
 if [ "$2" == "header" ]; then
     echo "System Logs"
 elif [ "$2" == "body" ]; then
-    echo "$(tail -n 25 /var/log/syslog)"
+    if [ -n "$(which journalctl)" ]; then
+        echo "$(journalctl -rn 25 --no-pager)"
+    elif [ -f /var/log/syslog ]; then
+        echo "$(tail -n 25 /var/log/syslog)"
+    elif [ -f /var/log/messages ]; then
+        echo "$(tail -n 25 /var/log/messages)"
+    else
+        echo "No syslog system detected"
+    fi
 fi
